7c1b3b8c440c3c18394feb97579b7b89791e10af
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-10-08 01:31:42 +0100

    added a database connection function

--- bin/www.js
+++ bin/www.js
@@ -4,6 +4,7 @@ const connectToDatabase = require('../server/database/connection');
 const startServer = async () => {
   const PORT = process.env.PORT || 8080;
   await connectToDatabase();
+
   app.listen(PORT, () => console.log('server started succesfully'));
 };
 
