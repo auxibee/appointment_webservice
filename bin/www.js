1e784a388df695994011a641379650c7ae5b574d
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-10-10 05:21:44 +0100

    removed connectToDatabase function

--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,7 @@
 const app = require('../app');
-const connectToDatabase = require('../server/database/connection');
 
 const startServer = async () => {
   const PORT = process.env.PORT || 8080;
-  await connectToDatabase();
 
   app.listen(PORT, () => console.log('server started succesfully'));
 };
