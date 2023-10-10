1c96f1b4614c544856ed11084691020269f5fe74
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-10-10 06:05:49 +0100

    restructing folder

--- server/controllers/health.js
+++
@@ -1,5 +0,0 @@
-async function health(req, res) {
-  res.json({ health: 'ok' });
-}
-
-module.exports = health;
--- server/routes/health.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const health = require('../controllers/health');
-
-const router = express.Router();
-
-router.get('/health', health);
-
-module.exports = router;
